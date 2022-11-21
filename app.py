e181589496b43ee131890c06a4a3787eddd55562
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2022-11-21 14:20:14 -0600

    feat(data): added data for algorithms and testing vectors

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, make_response
+
+from data import algorithms, testing_vectors
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/algorithms')
+def get_algorithms():
+    return make_response(algorithms.algorithms)
+
+
+@app.route('/testing_vectors/<string:type>')
+def get_testing_vectors(type):
+    try:
+        return make_response(testing_vectors.TESTING_VECTORS[type], 200)
+    except KeyError:
+        return make_response('Invalid type', 400)
 
 
 if __name__ == '__main__':
---
+++ data/algorithms.py
@@ -0,0 +1,61 @@
+algorithms = [
+    {
+        'name': 'chacha20',
+        'key_size': 256,
+        'description': 'test',
+    },
+    {
+        'name': 'aes-ebc',
+        'key_size': 256,
+        'description': ''
+    },
+    {
+        'name': 'aes-cbc',
+        'key_size': 256,
+        'description': ''
+    },
+    {
+        'name': 'sha-2',
+        'key_size': 384,
+        'description': ''
+    },
+    {
+        'name': 'sha-2-512',
+        'key_size': 512,
+        'description': ''
+    },
+    {
+        'name': 'sha-3',
+        'key_size': 384,
+        'description': ''
+    },
+    {
+        'name': 'sha-3-512',
+        'key_size': 512,
+        'description': ''
+    },
+    {
+        'name': 'rsa-oaep',
+        'key_size': 1024,
+        'description': ''
+    },
+    {
+        'name': 'rsa-pss',
+        'key_size': 1024,
+        'description': ''
+    },
+    {
+        'name': 'ecdsa-prime',
+        'key_size': 521,
+        'description': ''
+    },
+    {
+        'name': 'ecdsa-binary',
+        'key_size': 571,
+        'description': ''
+    }
+]
+
+# TODO: Add description to each algorithm
+
+# TODO: Add category to each algorithm (hash, cipher, signature, etc)

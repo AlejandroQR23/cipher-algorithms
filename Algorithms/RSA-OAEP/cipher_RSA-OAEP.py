149950eb910f4a7eebc3b06758b176e548920c87
Roberto RQ <ingreyesquijanoroberto@gmail.com>
2022-12-11 20:32:33 -0600

    file inclusion with structures

--- Algorithms/RSA-OAEP/cipher_RSA-OAEP.py
+++ Algorithms/RSA-OAEP/cipher_RSA-OAEP.py
@@ -2,10 +2,7 @@ import time
 import numpy as np
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-
-
-
-
+from Algorithms.structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values
 
 for i in range(iter):
   message = b'You can attack now!'

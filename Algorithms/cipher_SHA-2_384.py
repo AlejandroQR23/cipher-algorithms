1020f34a58d980b7c2124006284f5dc02db97234
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-23 20:10:45 -0600

    correction first test SHA2-384

--- Algorithms/cipher_SHA-2_384.py
+++ Algorithms/cipher_SHA-2_384.py
@@ -13,16 +13,16 @@ for i in range(150):
   T2 = time.time()
 
   hash_prom_list.append(T2 - T1)
-'''
+
 # Calculo y eliminacion de valores atipicos para el hash.
-q75,q25 = np.percentile(hash_prom_list,[75,25])                           #Aqui esta el error
+q75,q25 = np.percentile(hash_prom_list,[75,25])
 IQR = q75 - q25
 
 # Elimina los valores por encima del limite superior
 hash_prom_list = [i for i in hash_prom_list if i < (q75 + 1.5*IQR)]
 # Elimina los valores por debajo del limite inferior
 hash_prom_list = [i for i in hash_prom_list if i > (q25 - 1.5*IQR)]
-
-hash_values["SHA2-384"] = np.mean(hash_prom_list)
+'''
+hash_values["SHA2-384"] = np.mean(hash_prom_list)                         #Aqui esta el error
 hash_prom_list.clear()
 '''

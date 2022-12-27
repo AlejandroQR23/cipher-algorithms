e2f2f8113183d152456ebf4be1c9fc9a39439fcb
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-26 23:59:18 -0600

    average calculation

--- Algorithms/cipher_AES-ECB.py
+++ Algorithms/cipher_AES-ECB.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 from Crypto.Util.Padding import pad
 from Crypto.Cipher import AES
-from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values
+from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,average
 
 for i in range(150):
   data = b'secret'
@@ -13,8 +13,8 @@ for i in range(150):
   T1 = time.time()
   ct_bytes = cipher.encrypt(padded_block)
   T2 = time.time()
-
-  encryp_prom_list.append(T2 - T1)
+  
+  average = average+(T2-T1)
 
   try:
     T1 = time.time()
@@ -24,27 +24,5 @@ for i in range(150):
   except (ValueError, KeyError):
     print("Incorrect decryption")
 
-# Calculo y eliminacion de valores atipicos para la encriptacion.
-q75,q25 = np.percentile(encryp_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-encryp_prom_list = [i for i in encryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-encryp_prom_list = [i for i in encryp_prom_list if i > (q25 - 1.5*IQR)]
-'''
-encryp_values["AES-ECB"] = np.mean(encryp_prom_list)                       #Aqui esta el error
-encryp_prom_list.clear()
-
-# Calculo y eliminacion de valores atipicos para la desencriptacion.
-q75,q25 = np.percentile(decryp_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-decryp_prom_list = [i for i in decryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-decryp_prom_list = [i for i in decryp_prom_list if i > (q25 - 1.5*IQR)]
-
-decryp_values["AES-ECB"] = np.mean(decryp_prom_list)
-decryp_prom_list.clear()
-'''
+print(average)
+encryp_prom_list.append(average)

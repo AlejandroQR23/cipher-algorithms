55ea4ae421b8289f7b50b1469caf779d1c64f65e
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 00:09:36 -0600

    average calculation

--- Algorithms/cipher_Chacha20.py
+++ Algorithms/cipher_Chacha20.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import ChaCha20
 from Crypto.Random import get_random_bytes
 import time
 import numpy as np
-from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values
+from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,average
 
 for i in range(150):
   cha_plaintext = b'Texto prueba para el primer proyecto de criptografia'
@@ -12,7 +12,7 @@ for i in range(150):
   cha_ciphertext = cha_cipher.encrypt(cha_plaintext)
   T2 = time.time()
 
-  encryp_prom_list.append(T2 - T1)
+  average = average+(T2-T1)
 
   nonce = cha_cipher.nonce
 
@@ -26,27 +26,5 @@ for i in range(150):
 
   decryp_prom_list.append(T2 - T1)
 
-# Calculo y eliminacion de valores atipicos para la encriptacion.
-q75,q25 = np.percentile(encryp_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-encryp_prom_list = [i for i in encryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-encryp_prom_list = [i for i in encryp_prom_list if i > (q25 - 1.5*IQR)]
-'''
-encryp_values["Chacha20"] = np.mean(encryp_prom_list)                     #Aqui esta el error
-encryp_prom_list.clear()
-
-# Calculo y eliminacion de valores atipicos para la desencriptacion.
-q75,q25 = np.percentile(decryp_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-decryp_prom_list = [i for i in decryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-decryp_prom_list = [i for i in decryp_prom_list if i > (q25 - 1.5*IQR)]
-
-decryp_values["Chacha20"] = np.mean(decryp_prom_list)
-decryp_prom_list.clear()
-'''
+print(average)
+encryp_prom_list.append(T2 - T1)

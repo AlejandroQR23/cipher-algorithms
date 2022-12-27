ff72da6a117d8b797e6f6499e53b314f9ef1ccb5
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 00:22:55 -0600

    average calculation

--- Algorithms/cipher_RSA-OAEP.py
+++ Algorithms/cipher_RSA-OAEP.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values
+from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,average
 
 for i in range(150):
   message = b'You can attack now!'
@@ -14,7 +14,7 @@ for i in range(150):
   ciphertext = cipher.encrypt(message)
   T2 = time.time()
 
-  encryp_prom_list.append(T2 - T1)
+  average = average+(T2-T1)
 
   try:
     cipher = PKCS1_OAEP.new(keys)
@@ -24,27 +24,6 @@ for i in range(150):
     decryp_prom_list.append(T2 - T1)
   except(ValueError, TypeError):
     print("Incorrect decryption")
-  
-# Calculo y eliminacion de valores atipicos para la encriptacion.
-q75,q25 = np.percentile(encryp_prom_list,[75,25])
-IQR = q75 - q25
 
-# Elimina los valores por encima del limite superior
-encryp_prom_list = [i for i in encryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-encryp_prom_list = [i for i in encryp_prom_list if i > (q25 - 1.5*IQR)]
-
-encryp_values["RSA-OAEP"] = np.mean(encryp_prom_list)
-encryp_prom_list.clear()
-
-# Calculo y eliminacion de valores atipicos para la desencriptacion.
-q75,q25 = np.percentile(decryp_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-decryp_prom_list = [i for i in decryp_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-decryp_prom_list = [i for i in decryp_prom_list if i > (q25 - 1.5*IQR)]
-
-decryp_values["RSA-OAEP"] = np.mean(decryp_prom_list)
-decryp_prom_list.clear()
+print(average)
+encryp_prom_list.append(average)

6f424990bb761d14628cac85844f594897f8aa29
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 19:32:51 -0600

    average signature and verification

--- Algorithms/cipher_ECDSA-Binario.py
+++ Algorithms/cipher_ECDSA-Binario.py
@@ -3,9 +3,9 @@ import numpy as np
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.exceptions import InvalidSignature
-from structures import sign_prom_list,verify_prom_list,sign_values,verify_values,average
+from structures import sign_prom_list,verify_prom_list,sign_values,verify_values,VerifyAverage,signatureAverage,iter
 
-for i in range(150):
+for i in range(iter):
   llaveFirmaPrivada = ec.generate_private_key(ec.SECT571K1())
   llavePublica = llaveFirmaPrivada.public_key()
 
@@ -13,16 +13,21 @@ for i in range(150):
   firma = llaveFirmaPrivada.sign(b"message",ec.ECDSA(hashes.SHA256()))
   T2 = time.time()
 
-  average = average+(T2-T1)
+  signatureAverage = signatureAverage+(T2-T1)
 
   try:
     #Al igual que la de verificacion
     T1 = time.time()
     llavePublica.verify(firma,b"message",ec.ECDSA(hashes.SHA256()))
     T2 = time.time()
-    verify_prom_list.append( T2 - T1 )
+    
+    VerifyAverage = VerifyAverage+(T2-T1)
+
   except InvalidSignature:
     print("BAD SIGNATURE")
 
-print(average)
-sign_prom_list.append( T2 - T1 )
+print(signatureAverage/iter)
+sign_prom_list.append(signatureAverage/iter)
+
+print(VerifyAverage/iter)
+verify_prom_list.append(VerifyAverage/iter)

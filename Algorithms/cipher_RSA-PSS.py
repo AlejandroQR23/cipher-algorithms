8aa4b4d368e86cf3b46f36c0002986f862bc3876
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 19:47:25 -0600

    average signature and verification

--- Algorithms/cipher_RSA-PSS.py
+++ Algorithms/cipher_RSA-PSS.py
@@ -3,30 +3,33 @@ import numpy as np
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
-from structures import sign_prom_list,verify_prom_list,sign_values,verify_values,average
+from structures import sign_prom_list,verify_prom_list,sign_values,verify_values,signatureAverage,VerifyAverage,iter
 
-for i in range(150):
+for i in range(iter):
   message = b'To be signed'
   keys = RSA.generate(2048)
   private_key = keys.export_key()
   public_key = keys.publickey()
-
   h = SHA256.new(message)
-
   T1 = time.time()
   signature = pss.new(keys).sign(h)
   T2 = time.time()
 
-  average = average+(T2-T1)
+  signatureAverage = signatureAverage+(T2-T1)
 
   verifier = pss.new(keys)
   try:
     T1 = time.time()
     verifier.verify(h, signature)
     T2 = time.time()
-    verify_prom_list.append( T2 - T1 )
+
+    VerifyAverage = VerifyAverage+(T2-T1)
+    
   except (ValueError, TypeError):
     print ("The signature is not authentic.")
 
-print(average)
-sign_prom_list.append(average)
+print(signatureAverage/iter)
+sign_prom_list.append(signatureAverage/iter)
+
+print(VerifyAverage/iter)
+verify_prom_list.append(VerifyAverage/iter)

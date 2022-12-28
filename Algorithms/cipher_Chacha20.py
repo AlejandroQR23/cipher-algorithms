080308b33b773182479cb76c167929d9c9569a9b
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 19:21:26 -0600

    decryption calculation and fix in encryption calculation

--- Algorithms/cipher_Chacha20.py
+++ Algorithms/cipher_Chacha20.py
@@ -2,9 +2,9 @@ from Crypto.Cipher import ChaCha20
 from Crypto.Random import get_random_bytes
 import time
 import numpy as np
-from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,average
+from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,averageEncript,averageDecript,iter
 
-for i in range(150):
+for i in range(iter):
   cha_plaintext = b'Texto prueba para el primer proyecto de criptografia'
   cha_key = get_random_bytes(32)
   cha_cipher = ChaCha20.new(key=cha_key)
@@ -12,19 +12,22 @@ for i in range(150):
   cha_ciphertext = cha_cipher.encrypt(cha_plaintext)
   T2 = time.time()
 
-  average = average+(T2-T1)
+  averageEncript = averageEncript+(T2-T1)
 
   nonce = cha_cipher.nonce
-
   try:
     cha_cipher = ChaCha20.new(key=cha_key, nonce=nonce)
     T1 = time.time()
     cha_plaintext_bits = cha_cipher.decrypt(cha_ciphertext)
     T2 = time.time()
+
+    averageDecript = averageDecript+(T2-T1)
+
   except (ValueError, KeyError):
     print("Incorrect decryption")
 
-  decryp_prom_list.append(T2 - T1)
+print(averageEncript/iter)
+encryp_prom_list.append(averageEncript/iter)
 
-print(average)
-encryp_prom_list.append(T2 - T1)
+print(averageDecript/iter)
+decryp_prom_list.append(averageDecript/iter)

df38984dc3f393d1ab7d391196060c300baa0f7e
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 19:17:13 -0600

    decryption calculation and fix in encryption calculation

--- Algorithms/cipher_AES-GCM.py
+++ Algorithms/cipher_AES-GCM.py
@@ -2,9 +2,9 @@ from Crypto.Random import get_random_bytes
 import time
 import numpy as np
 from Crypto.Cipher import AES
-from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,average
+from structures import encryp_prom_list,decryp_prom_list,encryp_values,decryp_values,averageEncript,averageDecript,iter
 
-for i in range(150):
+for i in range(iter):
   header = b"header"
   data = b"secret"
   aes_gcm_key = get_random_bytes(16)
@@ -14,19 +14,23 @@ for i in range(150):
   aes_gcm_ciphertext, tag = aes_gcm_cipher.encrypt_and_digest(data)
   T2 = time.time()
 
-  average = (T2-T1)+average
+  averageEncript = averageEncript+(T2-T1)
 
   nonce = aes_gcm_cipher.nonce
-
   try:
       aes_gcm_cipher = AES.new(aes_gcm_key, AES.MODE_GCM, nonce)
       aes_gcm_cipher.update(header)
       T1 = time.time()
       plaintext = aes_gcm_cipher.decrypt_and_verify(aes_gcm_ciphertext, tag)
       T2 = time.time()
-      decryp_prom_list.append(T2 - T1)
+      
+      averageDecript = averageDecript+(T2-T1)
+
   except (ValueError, KeyError):
       print("Incorrect decryption")
 
-print(average)
-encryp_prom_list.append(T2 - T1)
+print(averageEncript/iter)
+encryp_prom_list.append(averageEncript/iter)
+
+print(averageDecript/iter)
+decryp_prom_list.append(averageDecript/iter)

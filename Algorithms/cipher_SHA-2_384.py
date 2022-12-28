9bd561aef3d262fbf98074745d8a227c79d12619
Roberto <ingreyesquijanoroberto@gmail.com>
2022-12-27 18:49:32 -0600

    average calculation

--- Algorithms/cipher_SHA-2_384.py
+++ Algorithms/cipher_SHA-2_384.py
@@ -1,9 +1,9 @@
 import time
 import numpy as np
 from Crypto.Hash import SHA384
-from structures import hash_prom_list,hash_values
+from structures import hash_prom_list,hash_values,average,iter
 
-for i in range(150):
+for i in range(iter):
   cadenaHash = b"MiCadenaHashSha2"
     
   # Creacion de objetos hash sha-2 y su codificacion
@@ -12,17 +12,7 @@ for i in range(150):
   hash_sha2_384.update( b"update")  # SHA384
   T2 = time.time()
 
-  hash_prom_list.append(T2 - T1)
+  average = average+(T2-T1)
 
-# Calculo y eliminacion de valores atipicos para el hash.
-q75,q25 = np.percentile(hash_prom_list,[75,25])
-IQR = q75 - q25
-
-# Elimina los valores por encima del limite superior
-hash_prom_list = [i for i in hash_prom_list if i < (q75 + 1.5*IQR)]
-# Elimina los valores por debajo del limite inferior
-hash_prom_list = [i for i in hash_prom_list if i > (q25 - 1.5*IQR)]
-'''
-hash_values["SHA2-384"] = np.mean(hash_prom_list)                         #Aqui esta el error
-hash_prom_list.clear()
-'''
+print(average/iter)
+hash_prom_list.append(average/iter)

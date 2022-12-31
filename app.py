9a131720c41e65d8947affb801e67e84f9928253
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2022-12-30 19:57:14 -0600

    feat(average): added endpoint to get avg time

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 from data import algorithms, testing_vectors
 
@@ -12,7 +12,31 @@ def hello_world():
 
 @app.route('/algorithms')
 def get_algorithms():
-    return make_response(algorithms.algorithms)
+    cipher_algorithms = [
+        {
+            'name': algorithm['name'],
+            'key_size': algorithm['key_size'],
+            'description': algorithm['description']
+        }
+        for algorithm in algorithms.algorithms
+    ]
+    return make_response(cipher_algorithms, 200)
+
+
+@app.route('/algorithms/average/<string:algorithm>')
+def get_average_time(algorithm):
+    iterations = int(request.args.get('iterations', 150))
+    data = request.args.get('data', 'secret')
+
+    for current_algorithm in algorithms.algorithms:
+        if current_algorithm['name'] == algorithm:
+            average_encrypt, average_decrypt = current_algorithm['get_avg'](
+                iterations, data)
+            return make_response({
+                'avg_encrypt': average_encrypt,
+                'avg_decrypt': average_decrypt
+            }, 200)
+    return make_response('Invalid algorithm', 400)
 
 
 @app.route('/testing_vectors/<string:type>')
